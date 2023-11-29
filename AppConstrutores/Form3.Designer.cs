2b6c6fce2bbe66c1438a9e7981f223b3f6861a8b
Tipimenta <tiagopimenta.ata@gmail.com>
2023-11-28 21:12:30 -0300

    Adicionar arquivos de projeto.

---
+++ AppConstrutores/Form3.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace AppConstrutores
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.radioconcreto = new System.Windows.Forms.RadioButton();
+            this.radioceramico = new System.Windows.Forms.RadioButton();
+            this.radiobaiano = new System.Windows.Forms.RadioButton();
+            this.radiobarro = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textaltura = new System.Windows.Forms.TextBox();
+            this.textlargura = new System.Windows.Forms.TextBox();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioconcreto
+            // 
+            this.radioconcreto.AutoSize = true;
+            this.radioconcreto.Checked = true;
+            this.radioconcreto.Location = new System.Drawing.Point(46, 98);
+            this.radioconcreto.Name = "radioconcreto";
+            this.radioconcreto.Size = new System.Drawing.Size(21, 20);
+            this.radioconcreto.TabIndex = 0;
+            this.radioconcreto.TabStop = true;
+            this.radioconcreto.UseVisualStyleBackColor = true;
+            this.radioconcreto.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioceramico
+            // 
+            this.radioceramico.AutoSize = true;
+            this.radioceramico.Location = new System.Drawing.Point(46, 153);
+            this.radioceramico.Name = "radioceramico";
+            this.radioceramico.Size = new System.Drawing.Size(21, 20);
+            this.radioceramico.TabIndex = 1;
+            this.radioceramico.UseVisualStyleBackColor = true;
+            this.radioceramico.CheckedChanged += new System.EventHandler(this.radioceramico_CheckedChanged);
+            // 
+            // radiobaiano
+            // 
+            this.radiobaiano.AutoSize = true;
+            this.radiobaiano.Location = new System.Drawing.Point(46, 204);
+            this.radiobaiano.Name = "radiobaiano";
+            this.radiobaiano.Size = new System.Drawing.Size(21, 20);
+            this.radiobaiano.TabIndex = 2;
+            this.radiobaiano.UseVisualStyleBackColor = true;
+            this.radiobaiano.CheckedChanged += new System.EventHandler(this.radiobaiano_CheckedChanged);
+            // 
+            // radiobarro
+            // 
+            this.radiobarro.AutoSize = true;
+            this.radiobarro.Location = new System.Drawing.Point(46, 248);
+            this.radiobarro.Name = "radiobarro";
+            this.radiobarro.Size = new System.Drawing.Size(21, 20);
+            this.radiobarro.TabIndex = 3;
+            this.radiobarro.UseVisualStyleBackColor = true;
+            this.radiobarro.CheckedChanged += new System.EventHandler(this.radiobarro_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(46, 293);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(21, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // textaltura
+            // 
+            this.textaltura.BackColor = System.Drawing.Color.Gainsboro;
+            this.textaltura.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textaltura.Location = new System.Drawing.Point(68, 422);
+            this.textaltura.Name = "textaltura";
+            this.textaltura.Size = new System.Drawing.Size(182, 19);
+            this.textaltura.TabIndex = 8;
+            // 
+            // textlargura
+            // 
+            this.textlargura.BackColor = System.Drawing.Color.Gainsboro;
+            this.textlargura.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textlargura.Location = new System.Drawing.Point(279, 422);
+            this.textlargura.Name = "textlargura";
+            this.textlargura.Size = new System.Drawing.Size(182, 19);
+            this.textlargura.TabIndex = 9;
+            // 
+            // textA
+            // 
+            this.textA.BackColor = System.Drawing.Color.White;
+            this.textA.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textA.Location = new System.Drawing.Point(143, 288);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(21, 19);
+            this.textA.TabIndex = 10;
+            // 
+            // textB
+            // 
+            this.textB.BackColor = System.Drawing.Color.White;
+            this.textB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textB.Location = new System.Drawing.Point(180, 288);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(21, 19);
+            this.textB.TabIndex = 11;
+            // 
+            // textC
+            // 
+            this.textC.BackColor = System.Drawing.Color.White;
+            this.textC.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textC.Location = new System.Drawing.Point(218, 288);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(21, 19);
+            this.textC.TabIndex = 12;
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.ForeColor = System.Drawing.Color.Green;
+            this.resultado.Location = new System.Drawing.Point(25, 646);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 26);
+            this.resultado.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(165, 513);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 34);
+            this.button1.TabIndex = 15;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(520, 706);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.textlargura);
+            this.Controls.Add(this.textaltura);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radiobarro);
+            this.Controls.Add(this.radiobaiano);
+            this.Controls.Add(this.radioceramico);
+            this.Controls.Add(this.radioconcreto);
+            this.DoubleBuffered = true;
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioconcreto;
+        private System.Windows.Forms.RadioButton radioceramico;
+        private System.Windows.Forms.RadioButton radiobaiano;
+        private System.Windows.Forms.RadioButton radiobarro;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textaltura;
+        private System.Windows.Forms.TextBox textlargura;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ AppConstrutores/Form3.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppConstrutores
+{
+    public partial class Form3 : Form
+    {
+        decimal areaTijolo = 741;
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            areaTijolo = 741;
+           
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioceramico_CheckedChanged(object sender, EventArgs e)
+        {
+            areaTijolo = 551;
+        }
+
+        private void radiobaiano_CheckedChanged(object sender, EventArgs e)
+        {
+            areaTijolo = 551;
+        }
+
+        private void radiobarro_CheckedChanged(object sender, EventArgs e)
+        {
+            areaTijolo = 105;
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            areaTijolo = Convert.ToDecimal(textB.Text) * Convert.ToDecimal(textC.Text);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal altura = Convert.ToDecimal(textaltura.Text);
+            decimal largura = Convert.ToDecimal(textlargura.Text);
+            decimal area = altura * largura;
+
+
+            string result = Convert.ToString(Math.Round(1.05m * (area / areaTijolo)) );
+            resultado.Text = String.Format("Para " +area+ " m² será necessário " + result + " unidades");
+        }
+    }
+}
